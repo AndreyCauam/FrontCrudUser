9f5d7bd3bb28ac980f223e113ad2a71d9613e197
Andrey Cauam <andreycauam@gmail.com>
2024-12-04 11:30:27 -0300

    create_types

---
+++ src/Helpers/types.ts
@@ -0,0 +1,17 @@
+export interface User {
+    name: string;
+    email: string;
+    birthdate: Date;
+}
+
+export interface Notification {
+    title: string;
+    message: string;
+    type: string;
+}
+
+export interface Variable {
+    name: string;
+    email: string;
+    birthdate: Date;
+}
